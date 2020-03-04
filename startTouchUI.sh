c2cd7439d1844055557899787e027dc3ec1f8de3
CalSchilling <CalSchilling@users.noreply.github.com>
2020-03-04 10:35:16 -0500

    Update startTouchUI.sh

--- startTouchUI.sh
+++ startTouchUI.sh
@@ -13,6 +13,7 @@ done
 sleep 5s
 sudo x11vnc
 
-DISPLAY=:0 chromium-browser -start-fullscreen http://192.168.1.8 &
+chromium-browser -start-fullscreen http://192.168.1.8 --display=:0 &
 sleep 10s
 xte "key F11" -x:0
+#xte requires xautomation to be installed
